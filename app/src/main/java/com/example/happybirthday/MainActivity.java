7ba07e4fb0aa882db743bc20922a8cccf9ddef96
jiale0912 <jllim-wt15@student.tarc.edu.my>
2019-05-27 13:47:03 +0800

    Happy Birthday

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.happybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Main Acitivity","Happy Birthday");
+        Log.i("MainActivity", "MainActivity layout is complete");
+
+    }
+}
